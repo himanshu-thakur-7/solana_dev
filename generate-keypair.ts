3824b35041197d4c24780a4d5e313a1b674511fe
himanshu-thakur-7 <himan7781@gmail.com>
2024-07-15 19:31:35 +0530

    completed key gen task

---
+++ generate-keypair.ts
@@ -0,0 +1,18 @@
+// import { Keypair } from "@solana/web3.js";
+// const keypair = Keypair.generate();
+// // console.log(`The public key is: ${keypair.publicKey.toBase58()}`);
+// // console.log("The secret key is : ", keypair.secretKey);
+// console.log(`✅ Generated keypair!`);
+// = Keypair.generate();
+// // console.log(`The public key is: ${keypair.publicKey.toBase58()}`);
+// // console.log("The secret key is : ", keypair.secretKey);
+
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(`The public key is: ${keypair.publicKey.toBase58()}`);
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`
+);
